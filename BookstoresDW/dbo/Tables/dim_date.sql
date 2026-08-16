5aefefb337151195a42dca631bcf0fb709bd21dc
Maria Gil <madchen.fur.immer@gmail.com>
2026-08-16 00:49:31 -0400

    Carga completamente el flujo del ETL de DimBook y el de DimAddress

---
+++ BookstoresDW/dbo/Tables/dim_date.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[dim_date] (
+    [DateKey]           INT           NOT NULL,
+    [FullDate]          DATE          NOT NULL,
+    [DayNumberOfWeek]   TINYINT       NOT NULL,
+    [DayNameOfWeek]     NVARCHAR (10) NOT NULL,
+    [DayNumberOfMonth]  TINYINT       NOT NULL,
+    [DayNumberOfYear]   SMALLINT      NOT NULL,
+    [WeekNumberOfYear]  TINYINT       NOT NULL,
+    [MonthName]         NVARCHAR (10) NOT NULL,
+    [MonthNumberOfYear] TINYINT       NOT NULL,
+    [CalendarQuarter]   TINYINT       NOT NULL,
+    [CalendarYear]      SMALLINT      NOT NULL,
+    [CalendarSemester]  TINYINT       NOT NULL,
+    CONSTRAINT [PK_dim_date] PRIMARY KEY CLUSTERED ([DateKey] ASC)
+);
+
---
+++ BookstoresDW/dbo/Tables/fact_sales.sql
@@ -0,0 +1,38 @@
+﻿CREATE TABLE [dbo].[fact_sales] (
+    [fact_id]              INT             IDENTITY (1, 1) NOT NULL,
+    [order_date_key]       INT             NOT NULL,
+    [customer_key]         INT             NOT NULL,
+    [book_key]             INT             NOT NULL,
+    [shipping_address_key] INT             NOT NULL,
+    [order_id]             INT             NOT NULL,
+    [line_id]              INT             NOT NULL,
+    [price]                DECIMAL (10, 2) NOT NULL,
+    [quantity]             INT             CONSTRAINT [df_fact_sales_quantity] DEFAULT ((1)) NOT NULL,
+    [total_amount]         DECIMAL (10, 2) NOT NULL,
+    CONSTRAINT [pk_fact_sales] PRIMARY KEY CLUSTERED ([fact_id] ASC),
+    CONSTRAINT [fk_fact_sales_dim_address] FOREIGN KEY ([shipping_address_key]) REFERENCES [dbo].[dim_address] ([address_key]),
+    CONSTRAINT [fk_fact_sales_dim_book] FOREIGN KEY ([book_key]) REFERENCES [dbo].[dim_book] ([book_key]),
+    CONSTRAINT [fk_fact_sales_dim_customer] FOREIGN KEY ([customer_key]) REFERENCES [dbo].[dim_customer] ([customer_key]),
+    CONSTRAINT [fk_fact_sales_dim_date] FOREIGN KEY ([order_date_key]) REFERENCES [dbo].[dim_date] ([DateKey])
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [idx_fact_sales_order_date]
+    ON [dbo].[fact_sales]([order_date_key] ASC);
+
+
+GO
+CREATE NONCLUSTERED INDEX [idx_fact_sales_customer]
+    ON [dbo].[fact_sales]([customer_key] ASC);
+
+
+GO
+CREATE NONCLUSTERED INDEX [idx_fact_sales_book]
+    ON [dbo].[fact_sales]([book_key] ASC);
+
+
+GO
+CREATE NONCLUSTERED INDEX [idx_fact_sales_address]
+    ON [dbo].[fact_sales]([shipping_address_key] ASC);
+
